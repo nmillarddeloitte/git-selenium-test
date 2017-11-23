1d5365dd29d37a3327574d3c138a3c2ac0dd91fa
Nicklas Millard <nmillard@deloitte>
2017-11-23 15:56:39 +0100

    update app.java

--- src/main/java/dk/nicklasmillard/App.java
+++ src/main/java/dk/nicklasmillard/App.java
@@ -3,19 +3,11 @@ package dk.nicklasmillard;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.lang.annotation.Target;
-
-/**
- * Hello world!
- *
- */
 public class App {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver", "C:\\users\\nmillard\\Desktop\\geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
-
-        System.out.println( "Hello World!" );
 
         driver.get("http://www.google.dk");
         System.out.println("Successfully opened web browser");
