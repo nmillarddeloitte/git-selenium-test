49ae45796140cee5bd12bd7ed78c5814d6a4f208
Nicklas Millard <nmillard@deloitte.dk>
2017-11-24 13:20:10 +0100

    changed back

--- src/test/java/dk/nicklasmillard/TestOpenBrowser.java
+++ src/test/java/dk/nicklasmillard/TestOpenBrowser.java
@@ -8,7 +8,7 @@ import org.testng.Assert;
 public class TestOpenBrowser {
 
     @Test
-    public void TestOpenFirefoxBrowser() {
+    public void TestOpenBrowser() {
         // Set geckodriver
         System.setProperty("webdriver.gecko.driver", "C:\\users\\nmillard\\Desktop\\geckodriver.exe");
         // Instantiate new firefox driver
